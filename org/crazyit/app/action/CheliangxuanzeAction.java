cb4cae472c9b104ce13a66a74f167fa1c8423561
010200022422 <505246471@qq.com>
2017-07-29 16:48:19 +0800

    test1

--- org/crazyit/app/action/CheliangxuanzeAction.java
+++ org/crazyit/app/action/CheliangxuanzeAction.java
@@ -32,7 +32,6 @@ public class CheliangxuanzeAction implements Action {
         setMap(ms.gettainewbyche(che_id,che,che_leibie,xiangmuid,evtcfg1,evtcfg2));
      //热按后map输出输出到表。
       //success 返回到选择页面
-////
         return SUCCESS;
     }
 
